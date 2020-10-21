f72f5edaaff6fa35f0b274a04d35f5d6c6966ec6
SevenCats <snowcolton@hotmail.com>
2020-10-21 00:45:28 -0400

    get scss modules working

---
+++ src/components/Home/HeroIntro.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './HeroIntro.module.scss';
+
+export default function HomeHeroIntro() {
+	return (
+		<section className={styles.container}>
+
+		</section>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { PageProps } from 'gatsby';
 import DocumentHead from '../components/DocumentHead';
 import Layout from '../components/Layout';
+import HeroIntro from '../components/Home/HeroIntro';
 
 export default function Home(props: PageProps) {
 	return (
 		<>
 			<DocumentHead title="Home" />
 			<Layout>
-				<h1>Home</h1>
+				<HeroIntro />
 			</Layout>
 		</>
 	);
